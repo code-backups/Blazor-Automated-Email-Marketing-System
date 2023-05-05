4c63a8b4ab45c31be5ca10164d2a5c0a5601beec
404bot <404bot@pm.me>
2023-05-05 20:41:57 +0100

    data model revision - app shell setup phase

--- Blazor-Automated-Email-Marketing-System/Models/Models.cs
+++ Blazor-Automated-Email-Marketing-System/Models/Models.cs
@@ -3,12 +3,10 @@
     public class Subscriber
     {
         public int Id { get; set; }
-        public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Country { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public string SegmentIdsCsv { get; set; }
+        public string EmailAddress { get; set; }
+        public string TagIdsCsv { get; set; }
     }
 
     public class Campaign
