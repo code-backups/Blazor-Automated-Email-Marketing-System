f100e0d087025b997c1019ff4b1fd7f7f0c29196
404bot <404bot@pm.me>
2023-05-27 23:01:20 +0100

    Selenium deactivated

--- Blazor-Automated-Email-Marketing-System.Tests/StagingTests.cs
+++ Blazor-Automated-Email-Marketing-System.Tests/StagingTests.cs
@@ -1,52 +1,30 @@
+using Blazor_Automated_Email_Marketing_System.Models;
 using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
 
-namespace Blazor_Automated_Email_Marketing_System.Tests;
-
-public class StagingTests
+namespace Blazor_Automated_Email_Marketing_System.Tests
 {
-    private IWebDriver _driver;
-
-    [SetUp]
-    public void Setup()
+    public class StagingTests
     {
-#if GITHUB_ACTIONS
-            // Use Chromium binary in Ubuntu runner for GitHub Actions
-            var chromeOptions = new ChromeOptions();
-            chromeOptions.BinaryLocation = "/usr/bin/chromium-browser";
-            var chromeDriverService = ChromeDriverService.CreateDefaultService();
-            chromeDriverService.UseSpecCompliantProtocol = true;
-            _driver = new ChromeDriver(chromeDriverService, chromeOptions);
-#else
-        // Use Chrome driver on local machine
-        _driver = new ChromeDriver();
-#endif
-        // Maximize the browser window
-        _driver.Manage().Window.Maximize();
-    }
+        // Simple integration test verifies interaction between different components of Subscriber model - first name and last name to create a correct full name.
 
-    [Test]
-    [Category("Integration")]
-    public void BrowserIntegration_CheckPageTitle()
-    {
-        // Arrange
-        var expectedTitle = "AutoEMS";
-        // Act
-        _driver.Navigate().GoToUrl("https://localhost:7160/");
-        // Wait until the page title contains the expected value
-        var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
-        wait.Until(d => d.Title.Contains(expectedTitle));
-        // Assert
-        Assert.AreEqual(expectedTitle, _driver.Title);
-    }
+        [Test]
+        [Category("Integration")]
+        public void Models_Integration()
+        {
+            // Arrange - Create an instance of the Subscriber model
+            var subscriber = new Subscriber
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                EmailAddress = "johndoe@notadomain.com",
+                TagIdsCsv = "1,2,3"
+            };
+            // Act - Combine firstname, lastname of Subscriber - create full name
+            var fullName = $"{subscriber.FirstName} {subscriber.LastName}";
 
-    [TearDown]
-    public void TearDown()
-    {
-        // Quit the driver and dispose of its resources
-        _driver.Quit();
-        _driver.Dispose();
+            // Assert - Verify that the full name is equal to "John Doe"
+            Assert.AreEqual("John Doe", fullName);
+        }
     }
 }
